34cbfb6e96b85294671b893b1e419ae999adb60d
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-26 22:35:28 -1000

    Update finalprojectutil.h

--- finalprojectutil.h
+++ finalprojectutil.h
@@ -1,1 +1,22 @@
+/*
+**************************
+File Name:	    finalprojectutil.h
+Created By:	    Jonah Bobilin
+Login:          jbobilin
+Team:		        Undercooked Dinosaurs
+Date Created:	  26 November 2017
+Last Modified:	26 November 2017
+Description:	  utility functions, macros, etc. for the Undercooked Dinosaurs's final project in ee160
+**************************
+*/
 
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+#define TRUE 1
+#define FALSE 0
+
+/*	***  Declare Prototypes  ***	*/
+//note: rn there are no utility functions, if you add one, please write a finalproject.c file to put it in and modify the README.txt file associated with the project to reflect your changes.
