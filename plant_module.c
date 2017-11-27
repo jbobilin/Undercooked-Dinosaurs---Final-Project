d75ba886085602dd76efe24cd23ea24ec64ab4c4
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-26 23:19:40 -1000

    Create plant_module.c

---
+++ plant_module.c
@@ -0,0 +1,43 @@
+/*
+**************************
+File Name:      plant_module.c
+Created By:     
+Login:          
+Team:           Undercooked Dinosaurs
+Date Created:   26 November 2017
+Last Modified:  26 November 2017
+Description:	  
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+
+
+/*	***  Declare Prototypes  ***	*/
+
+
+
+/*	***  Main Code  ***	*/
+int main(void)
+{
+	// declare variables
+	
+	
+	// initialize variables
+	
+	
+}
+
+
+
+/*	***  Functions Workspace  ***	*/
+
+/* Function Name:  
+ * I/O:  /  
+ * Purpose/Notes:  */
+
