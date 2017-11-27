c05a5197fb02db9a68670e1d1ada7e60073a0798
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-26 23:10:02 -1000

    Create mainmodule.c

---
+++ mainmodule.c
@@ -0,0 +1,45 @@
+/*
+**************************
+File Name:      mainmodule.c
+Created By:     Jonah Bobilin
+Login:          jbobilin
+Team:           Undercooked Dinosaurs
+Date Created:   26 November 2017
+Last Modified:  26 November 2017
+Description:	  
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+
+
+/*	***  Declare Prototypes  ***	*/
+
+
+
+/*	***  Main Code  ***	*/
+int main(void)
+{
+	// declare variables
+	
+	
+	// initialize variables
+	
+	
+}
+
+
+
+/*	***  Functions Workspace  ***	*/
+
+/* Function Name:  
+ * I/O:  /  
+ * Purpose/Notes:  */
+
+
+
