d69dfe5630db974180804a23eea92fc5bc745b1c
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-26 23:12:16 -1000

    Create starting_configuration_module.h

---
+++ starting_configuration_module.h
@@ -0,0 +1,23 @@
+/*
+**************************
+File Name:      starting_configuration_module.h
+Created By:     Jonah Bobilin
+Login:          jbobilin
+Team:           Undercooked Dinosaurs
+Date Created:   26 November 2017
+Last Modified:  26 November 2017
+Description:	  
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+
+
+/*	***  Declare Prototypes  ***	*/
+
+
