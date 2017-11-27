35713e5dfa3ddc4c8f7b6ed6995ea23bbd1518ab
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-26 23:10:43 -1000

    Create mainmodule.h

---
+++ mainmodule.h
@@ -0,0 +1,22 @@
+/*
+**************************
+File Name:      mainmodule.h
+Created By:     Jonah Bobilin
+Login:          jbobilin
+Team:           Undercooked Dinosaurs
+Date Created:   26 November 2017
+Last Modified:  26 November 2017
+Description:	  
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+
+
+/*	***  Declare Prototypes  ***	*/
+
