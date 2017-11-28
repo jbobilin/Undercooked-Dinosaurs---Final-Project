42f87956efaf35e1bc6162f3d5fd3956603692d9
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-28 11:40:03 -1000

    Update main_module.c

--- main_module.c
+++ main_module.c
@@ -1,8 +1,8 @@
 /*
 **************************
 File Name:      main_module.c
-Created By:     
-Login:          
+Created By:     Jonah Bobilin
+Login:          jbobilin
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
 Last Modified:  26 November 2017
@@ -12,11 +12,19 @@ Description:
 
 /*	***  Call Header Files  ***	*/
 #include <stdio.h>
-
+#include "display.h"
+#include "starting_configuration_module.h"
+#include "gameplay_module.h"
+#include "miensfeldutil.h"
 
 /*	***  Define Compiler Directives  ***	*/
 //#define DEBUG
 
+/*	***  Declare Global Variables  ***	*/
+int quit_flag = FALSE;
+enum TIM_LOC timmy_level[10][12];
+enum Glif display_level[8][10];
+enum Glif mine_level[8][10];
 
 /*	***  Declare Prototypes  ***	*/
 
