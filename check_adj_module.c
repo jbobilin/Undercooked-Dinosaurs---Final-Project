abc3dbad19aea33c6546d72e5f4513979eaf6b6d
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 12:44:56 -1000

    Update check_adj_module.c

--- check_adj_module.c
+++ check_adj_module.c
@@ -5,22 +5,38 @@ Created By:     Jonah Bobilin
 Login:          jbobilin
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
-Last Modified:  26 November 2017
-Description:	  
+Last Modified:  30 November 2017
+Description:	  This file contains the function adj.
 **************************
 */
 
 /*	***  Call Header Files  ***	*/
 #include <stdio.h>
 
-
-/*	***  Define Compiler Directives  ***	*/
-//#define DEBUG
-
-
 /*	***  Functions Workspace  ***	*/
 
-/* Function Name:  
- * I/O:  /  
- * Purpose/Notes:  */
-
+/* Function Name:  adj
+ * I/O:  int row, int column /  int
+ * Purpose/Notes:  This function is given a cell on the playing field and calculates the mines in all adjacent tiles*/
+int adj(int row, int column)
+{
+    int count=0;
+    if (Glif MINE == mine_level[row - 1, column - 1])
+        count++;
+    if (Glif MINE == mine_level[row - 1, column])
+        count++;
+    if (Glif MINE == mine_level[row - 1, column + 1])
+        count++;
+    if (Glif MINE == mine_level[row, column - 1])
+        count++;
+    if (Glif MINE == mine_level[row, column + 1])
+        count++;
+    if (Glif MINE == mine_level[row + 1, column - 1])
+        count++;
+    if (Glif MINE == mine_level[row + 1, column])
+        count++;
+    if (Glif MINE == mine_level[row + 1, column + 1])
+            count++;
+ 
+    return count;
+}
