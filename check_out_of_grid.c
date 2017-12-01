34c4c1411caf61ee02745ee6a3a14afd8c7c3819
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 14:19:12 -1000

    Create check_out_of_grid.c

---
+++ check_out_of_grid.c
@@ -0,0 +1,24 @@
+/*
+**************************
+File Name:	    check_out_of_grid.c
+Created By:	    Jonah Bobilin
+Login:          jbobilin
+Team:		        Undercooked Dinosaurs
+Date Created:	  26 November 2017
+Last Modified:	26 November 2017
+Description:	  This function is given a row and column and checks if the cell is within the playing area it returns
+								0 - OUT_OF_BOUNDS
+								1 - IN_FIELD
+								2 - WIN_ZONE
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <meinsfeldutil.h>
+
+
+/*	***  Functions Workspace  ***	*/
+int check_out_of_grid(int row, int column)
+{
+	
+}
