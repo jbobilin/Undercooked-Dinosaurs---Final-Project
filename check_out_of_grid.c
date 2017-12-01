2bc9522358de9b0865651c8c0a724bf0814c2e01
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 14:25:39 -1000

    Update check_out_of_grid.c

--- check_out_of_grid.c
+++ check_out_of_grid.c
@@ -1,15 +1,15 @@
 /*
 **************************
-File Name:	    check_out_of_grid.c
-Created By:	    Jonah Bobilin
+File Name:	check_out_of_grid.c
+Created By:	Jonah Bobilin
 Login:          jbobilin
-Team:		        Undercooked Dinosaurs
-Date Created:	  26 November 2017
+Team:		Undercooked Dinosaurs
+Date Created:	26 November 2017
 Last Modified:	26 November 2017
-Description:	  This function is given a row and column and checks if the cell is within the playing area it returns
-								0 - OUT_OF_BOUNDS
-								1 - IN_FIELD
-								2 - WIN_ZONE
+Description:	This function is given a row and column and checks if the cell is within the playing area it returns
+		0 - OUT_OF_BOUNDS
+		1 - IN_FIELD
+		2 - WIN_ZONE
 **************************
 */
 
