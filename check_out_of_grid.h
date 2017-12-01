164a49a923232e14a3e63f1e0fba1f6dba85648c
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-01 09:53:36 -1000

    Update check_out_of_grid.h

--- check_out_of_grid.h
+++ check_out_of_grid.h
@@ -1,18 +1,17 @@
 /*
 **************************
-File Name:	    check_out_of_grid.h
-Created By:	    Jonah Bobilin
-Login:          jbobilin
-Team:		        Undercooked Dinosaurs
-Date Created:	  30 November 2017
+File Name:	check_out_of_grid.h
+Created By:	Jonah Bobilin
+Login:		jbobilin
+Team:		Undercooked Dinosaurs
+Date Created:	30 November 2017
 Last Modified:	30 November 2017
-Description:	  this file contains the prototype check_out_of_grid()
+Description:	this file contains the prototype check_out_of_grid()
 **************************
 */
 
 /*	***  Call Header Files  ***	*/
 #include <meinsfeldutil.h>
-
 
 /*	***  Declare Prototypes  ***	*/
 int check_out_of_grid(int row, int column);
