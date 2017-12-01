b878ef14ddf2524735a4e548c24848f1867c3ce7
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 17:25:10 -1000

    Update gameplay_module.c

--- gameplay_module.c
+++ gameplay_module.c
@@ -20,7 +20,69 @@ Description:
 
 /*	***  Functions Workspace  ***	*/
 
-/* Function Name:  
- * I/O:  /  
- * Purpose/Notes:  */
-
+/* Function Name:  gameplay_module
+ * I/O:  void / void  
+ * Purpose/Notes:  this module runs the game as it is played by the human*/
+void gameplay_module(void)
+{
+	while (quit_flag == FALSE)
+	{
+		char user_input;
+		user_input = getchar();
+		switch (user_input)
+		{
+			case 'y':			//move up left
+				move(-1, -1);
+				break;
+			case 'u':			//move up
+				move(-1, 0);
+				break;
+			case 'i':			//move up right
+				move(-1, 1);
+				break;
+			case 'h':			//move left
+				move(0, -1);
+				break;
+			case 'k':			//move right
+				move(0, 1);
+				break;
+			case 'n':			//move down left
+				move(1, -1);
+				break;
+			case 'm':			//move down
+				move(1, 0);
+				break;
+			case ',':			//move down right
+				move(1, 1);
+				break;
+			case 'Y':			//plant up left
+				plant_flag(-1, -1);
+				break;
+			case 'U':			//plant up
+				plant_flag(-1, 0);
+				break;
+			case 'I':			//plant up right
+				plant_flag(-1, 1);
+				break;
+			case 'H':			//plant left
+				plant_flaf(0, -1);
+				break;
+			case 'K':			//plant right
+				plant_flag(0, 1);
+				break;
+			case 'N':			//plant down left
+				plant_flag(1, -1);
+				break;
+			case 'M':			//plant down
+				plant_flag(1, 0);
+				break;
+			case '<':			//plant down right
+				plant_flag(1, 1)
+				break;
+			case 'Q':
+			case 'q':
+				quit_sequence();
+			default:
+		}
+	}
+}
