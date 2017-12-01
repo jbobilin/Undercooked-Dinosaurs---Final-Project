702cf58f373bf00216eaf2d9ec923aa9bef11190
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 17:26:31 -1000

    Update gameplay_module.h

--- gameplay_module.h
+++ gameplay_module.h
@@ -1,12 +1,12 @@
 /*
 **************************
 File Name:      gameplay_module.h
-Created By:     
-Login:          
+Created By:     Jonah Bobilin
+Login:          jbobilin
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
-Last Modified:  26 November 2017
-Description:	  
+Last Modified:  30 November 2017
+Description:	  this file contains the prototype for the gameplay module
 **************************
 */
 
@@ -15,5 +15,4 @@ Description:
 
 
 /*	***  Declare Prototypes  ***	*/
-
-
+void gameplay_module(void);
