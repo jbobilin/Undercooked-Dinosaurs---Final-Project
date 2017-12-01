d772ec53ff6ce1e0d0c51bc4da0bfd92e1d623ed
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 14:09:52 -1000

    Create messages.h

---
+++ messages.h
@@ -0,0 +1,20 @@
+/*
+**************************
+File Name:	    messages.h
+Created By:	    Jonah Bobilin
+Login:          jbobilin
+Team:		        Undercooked Dinosaurs
+Date Created:	  26 November 2017
+Last Modified:	26 November 2017
+Description:	  utility functions, macros, etc. for the Undercooked Dinosaurs's final project in ee160
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include "miensfeldutil.h"
+
+
+/*	***  Messages Library  ***	*/
+char error_move[25] = “
+char error_flag[25] = “Cannot place a flag there\n you’ll lose it”;
+Play_again[25] = “Play again? \nWe’re gonna need another Timmy!”;
