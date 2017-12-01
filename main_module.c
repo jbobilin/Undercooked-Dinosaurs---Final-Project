45bab0869bf0b6024308745f8de983daee6bc22e
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-11-30 15:47:16 -1000

    Update main_module.c

--- main_module.c
+++ main_module.c
@@ -23,7 +23,10 @@ Description:	This is the main module for the Miensfeld game. It contains all the
 
 /*	***  Declare Global Variables  ***	*/
 int quit_flag = FALSE;
-timmys_location[1];
+int timmys_location[1];
+int score_count;
+int flags_count;
+int mines_count;
 enum TIM_LOC timmy_level[7][9];
 enum Glif display_level[7][9];
 enum Glif mine_level[7][9];
