ec3cbe69d80fbc2cf35f1ad344d2a87dd2918ae3
kuryn <34013423+kuryn@users.noreply.github.com>
2017-12-07 12:19:04 -1000

    Update plant_module.c

--- plant_module.c
+++ plant_module.c
@@ -1,11 +1,11 @@
 /*
 **************************
-File Name:      plant_module.c
-Created By:     
-Login:          
+File Name:      move_module.c
+Created By:     Thomas Yang
+Login:          thomasy4
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
-Last Modified:  26 November 2017
+Last Modified:  3 December 2017
 Description:	  
 **************************
 */
@@ -13,14 +13,61 @@ Description:
 /*	***  Call Header Files  ***	*/
 #include <stdio.h>
 
-
+ 
 /*	***  Define Compiler Directives  ***	*/
 //#define DEBUG
 
 
 /*	***  Functions Workspace  ***	*/
+    
+void move(int a, int b)
+{
 
-/* Function Name:  
- * I/O:  /  
- * Purpose/Notes:  */
+        int temp_row, temp_col, adjacent;
+        temp_row = timmys_location[0] + a;
+        temp_col = timmys_location[1] + b;
 
+        if (check_out_of_grid(temp_row, temp_col) == OUT_OF_BOUNDS)
+                write_message(15, "You can’t plant there!");
+        else if (check_out_of_grid(temp_row, temp_col) == WIN_ZONE)
+                win();
+        else if (check_out_of_grid(temp_row, temp_col) == IN_FIELD)
+        {
+                switch ( mine_level[temp_row, temp_col])
+                {
+                        case MINE:
+                        case FL_MINE:
+                                death();
+                                break;
+                        case SAFE:
+                        case FLAG:
+                                if(timmy_level[temp_row,temp_col] == TIM_NOT_BEEN)
+                                {
+                                score_count++; 
+                                update_score(score_count);
+                                }
+                                timmy_level[timmys_location[0], timmys_location[1]] = TIM_BEEN;
+                                timmy_level[temp_row, temp_col] = TIM_ON;
+                                adjacent = adj(temp_row, temp_col);
+                                if (mine_level[timmys_location[0], timmys_location[1]] == FLAG)
+                                {
+                                        show_glif(Glif FLAG, temp_row, temp_col, 0);
+                                        display_level[timmys_location[0], timmys_location[1]] = FLAG;
+                                }
+                                if (mine_level[timmys_location[0], timmys_location[1]] == SAFE)
+                                {
+                                        show_glif(Glif SAFE, temp_row, temp_col, adj);
+                                        display_level[timmys_location[0], timmys_location[1]] = SAFE;
+                                }
+                                show_glif(Glif TIMMY, temp_row, temp_col, adj);
+                                display_level[temp_row, temp_col] = TIMMY;
+
+                                timmys_location[0] += a;
+                                timmys_location[1] += b;
+                                break;
+                        default:
+                                write_message("15, Error! The program messed up\n");
+                                break;
+                }
+        }
+}
