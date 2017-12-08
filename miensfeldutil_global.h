77698a964dcecdab0ea81ded76cbca9b28b0cc45
kuryn <34013423+kuryn@users.noreply.github.com>
2017-12-07 17:08:55 -1000

    Create miensfeldutil_global.h

---
+++ miensfeldutil_global.h
@@ -0,0 +1,31 @@
+/*
+**************************
+File Name:	miensfeldutil.h
+Created By:	Jonah Bobilin
+Login:          jbobilin
+Team:		Undercooked Dinosaurs
+Date Created:	26 November 2017
+Last Modified:	26 November 2017
+Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's final project in ee160
+**************************
+*/
+
+/*	***  Call Header Files  ***	*/
+#include <stdio.h>
+#include <string.h>
+
+/*	***  Define Compiler Directives  ***	*/
+//#define DEBUG
+#define TRUE           1
+#define FALSE          0
+#define RESTART        3
+#define OUT_OF_BOUNDS  4
+#define IN_FIELD       5
+#define WIN_ZONE       6
+
+/*	***  Declare Enummerated Types  ***	*/
+extern enum TIM_LOC {TIM_NOT_BEEN, TIM_ON, TIM_BEEN};
+extern typedef enum TIM_LOC;
+
+/*	***  Declare Prototypes  ***	*/
+//note: rn there are no utility functions, if you add one, please write a finalproject.c file to put it in and modify the README.txt file associated with the project to reflect your changes.
