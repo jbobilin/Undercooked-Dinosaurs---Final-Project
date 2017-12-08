9b691491631bf4e67e03707a9f4a3bfa3c85033b
kuryn <34013423+kuryn@users.noreply.github.com>
2017-12-07 16:15:04 -1000

    Update endgame_module.c

--- endgame_module.c
+++ endgame_module.c
@@ -15,6 +15,7 @@ Description:
 #include "main_global.h"
 #include "display.h"
 #include "miensfeldutil.h"
+#include "check_adj_module.h"
 
 /*	***  Define Compiler Directives  ***	*/
 //#define DEBUG
@@ -34,13 +35,44 @@ void death(void)
         show_glif(MINE,i,j,0);
       }
     }
+ }
   show_glif(EXPLODE,timmys_location[0],timmys_location[1],0);
   write_message(15, "We're gonna need another Timmy!");
   quit_sequence();
-  }
+ }
 void win(void)
 {
-  
+  int i, j, adjacent;
+  for(i=0; i<8; i++)
+  {
+    for(j=0; j<10; j++)
+    {
+      if(mine_level[i][j] == MINE)
+      {
+        show_glif(MINE,i,j,0);
+      }
+    }
+  }
+  adjacent = adj(timmys_location[0],timmys_location[1]);
+  show_glif(SAFE,timmys_location[0],timmys_location[1],adjacent);
+  write_message(15, "Cognratuations! You won!");
+  quit_sequence;
+}
+    
+ void quit_sequence(void)
+ {
+   int char;
+   char = getchar();
+   if(char == 'y' || char == 'Y') quit_flag = RESTART;
+   if(char == 'n' || char == 'N') quit_flag = TRUE;
+   
+   while(char != 'y' && char != 'Y' && char != 'N' && char != 'n')
+   {
+   char = getchar();
+   if(char == 'y' || char == 'Y') quit_flag = RESTART;
+   if(char == 'n' || char == 'N') quit_flag = TRUE;
+   }
+ }
   
 /* Function Name:  
  * I/O:  /  
