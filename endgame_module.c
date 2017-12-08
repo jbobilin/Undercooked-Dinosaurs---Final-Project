0cb27a0afe24ec0b5e6366766b60374044165b46
kuryn <34013423+kuryn@users.noreply.github.com>
2017-12-07 15:59:38 -1000

    Update endgame_module.c

--- endgame_module.c
+++ endgame_module.c
@@ -12,14 +12,36 @@ Description:
 
 /*	***  Call Header Files  ***	*/
 #include <stdio.h>
-
+#include "main_global.h"
+#include "display.h"
+#include "miensfeldutil.h"
 
 /*	***  Define Compiler Directives  ***	*/
 //#define DEBUG
 
+void quit_sequence(void);
 
 /*	***  Functions Workspace  ***	*/
-
+void death(void)
+{
+  int i, j;
+  for(i=0; i<8; i++)
+  {
+    for(j=0; j<10; j++)
+    {
+      if(mine_level[i][j] == MINE)
+      {
+        show_glif(MINE,i,j,0);
+      }
+    }
+  show_glif(EXPLODE,timmys_location[0],timmys_location[1],0);
+  write_message(15, "We're gonna need another Timmy!");
+  quit_sequence();
+  }
+void win(void)
+{
+  
+  
 /* Function Name:  
  * I/O:  /  
  * Purpose/Notes:  */
