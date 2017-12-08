84efeb7cdcffd5b1d21b2f6c7d26aa508b86332c
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-07 17:32:48 -1000

    Update print_shit.h

--- print_shit.h
+++ print_shit.h
@@ -1,1 +1,17 @@
+/*
+**************************
+File Name:      print_shit.h
+Created By:     Jonah Bobilin
+Login:          jbobilin
+Team:           Undercooked Dinosaurs
+Date Created:   7 December 2017
+Last Modified:  7 December 2017
+Description:	This is a file designed to debug to meinsfeld game.
+**************************
+*/
+
+/*	***  Declare Prototypes  ***	*/
+/* Function Name:  print_shit
+ * I/O:  void / void  
+ * Purpose/Notes:  this function prints all the global arrays to test if the game is correct*/
 void print_shit(void);
