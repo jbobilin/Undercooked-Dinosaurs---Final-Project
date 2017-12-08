e155fa43a60cb80d778762543c986d31a50ab0a4
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-07 18:28:33 -1000

    Update endgame_module.h

--- endgame_module.h
+++ endgame_module.h
@@ -1,18 +1,29 @@
 /*
 **************************
 File Name:      endgame_module.h
-Created By:     
-Login:          
+Created By:     Thomas Yang
+Login:          thomasy4
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
 Last Modified:  1 December 2017
-Description:	  
+Description:	This module contains the prototypes for a win, loss, and a prompt to play again.
 **************************
 */
 
 
 /*	***  Declare Prototypes  ***	*/
 
-/* Function Name:  
- * I/O:  /  
- * Purpose/Notes:  */
+/* Function Name:  death
+ * I/O:  void / void
+ * Purpose/Notes:  runs the death sequence*/
+void death(void);
+
+/* Function Name:  win
+ * I/O:  void / void
+ * Purpose/Notes:  runs the win sequence*/
+void win(void);
+
+/* Function Name:  quit_sequence
+ * I/O:  void / void
+ * Purpose/Notes:  prompts user to quit or play again*/
+ void quit_sequence(void);
