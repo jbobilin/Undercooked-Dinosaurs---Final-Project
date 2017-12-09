753008f848e3adbef0943a4238f5c5782fac9b29
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 01:01:37 -1000

    Update miensfeldutil.h

--- miensfeldutil.h
+++ miensfeldutil.h
@@ -5,7 +5,7 @@ Created By:	Jonah Bobilin
 Login:          jbobilin
 Team:		Undercooked Dinosaurs
 Date Created:	26 November 2017
-Last Modified:	26 November 2017
+Last Modified:	1 December 2017
 Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's final project in ee160
 **************************
 */
@@ -15,7 +15,7 @@ Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's fin
 #include <string.h>
 
 /*	***  Define Compiler Directives  ***	*/
-#define TRUE           1
+#define	TRUE           1
 #define FALSE          0
 #define RESTART        3
 #define OUT_OF_BOUNDS  4
@@ -23,8 +23,6 @@ Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's fin
 #define WIN_ZONE       6
 
 /*	***  Declare Enummerated Types  ***	*/
+//user defined type for timmy_level array
 enum TIM_LOC {TIM_NOT_BEEN, TIM_ON, TIM_BEEN};
 typedef enum TIM_LOC TIM_LOC;
-
-/*	***  Declare Prototypes  ***	*/
-//note: rn there are no utility functions, if you add one, please write a finalproject.c file to put it in and modify the README.txt file associated with the project to reflect your changes.
