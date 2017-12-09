5951899a220c982361063ccc12df9aa8a0edda3f
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 00:34:51 -1000

    Update gameplay_module.h

--- gameplay_module.h
+++ gameplay_module.h
@@ -6,10 +6,13 @@ Login:          jbobilin
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
 Last Modified:  30 November 2017
-Description:	  this file contains the prototype for the gameplay module
+Description:	this file contains the prototype for the gameplay module.
 **************************
 */
 
-
 /*	***  Declare Prototypes  ***	*/
+
+/* Function Name:  gameplay_module
+ * I/O:  void / void  
+ * Purpose/Notes:  this module runs the game as it is played by the human*/
 void gameplay_module(void);
