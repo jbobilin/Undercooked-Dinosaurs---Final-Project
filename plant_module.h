1a5b87e8cc8fe710a294786588df5df1256b3f35
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 01:57:04 -1000

    Update plant_module.h

--- plant_module.h
+++ plant_module.h
@@ -1,17 +1,18 @@
 /*
 **************************
 File Name:      plant_module.h
-Created By:     
-Login:          
+Created By:     Lian Huang
+Login:          lian7
 Team:           Undercooked Dinosaurs
-Date Created:   26 November 2017
-Last Modified:  26 November 2017
-Description:	  
+Date Created:   1 December 2017
+Last Modified:  1 December 2017
+Description:	this file contains the prototype for the plant_flag function.
 **************************
 */
 
-/*	***  Call Header Files  ***	*/
-
 /*	***  Declare Prototypes  ***	*/
 
+/* Function Name:  plant_flag
+ * I/O:  int a, int b / void
+ * Purpose/Notes:  This function plants a flag at the passed direction (a = row, b = column) if the plant space is valid, and updates the scores*/
 void plant_flag(int a, int b);
