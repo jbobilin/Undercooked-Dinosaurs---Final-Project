f7affa4f466278e6f22cd6ccfad7861054576afa
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 01:02:50 -1000

    Update miensfeldutil_global.h

--- miensfeldutil_global.h
+++ miensfeldutil_global.h
@@ -5,7 +5,7 @@ Created By:	Jonah Bobilin
 Login:          jbobilin
 Team:		Undercooked Dinosaurs
 Date Created:	26 November 2017
-Last Modified:	26 November 2017
+Last Modified:	9 December 2017
 Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's final project in ee160
 **************************
 */
@@ -22,16 +22,7 @@ Description:	utility functions, macros, etc. for the Undercooked Dinosaurs's fin
 #define IN_FIELD       5
 #define WIN_ZONE       6
 
-/*	***  Declare Enummerated Types  ***	*/
-//extern enum TIM_LOC {TIM_NOT_BEEN, TIM_ON, TIM_BEEN};
-//extern typedef enum TIM_LOC;
-
-
 /*	***  Messages Library  ***	*/
 extern char error_move[25];
 extern char error_flag[60];
 extern char play_again[60];
-
-
-/*	***  Declare Prototypes  ***	*/
-//note: rn there are no utility functions, if you add one, please write a finalproject.c file to put it in and modify the README.txt file associated with the project to reflect your changes.
