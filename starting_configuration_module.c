d315e522fa7206dc7984d0e57b2739c45955f2a2
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-08 23:03:07 -1000

    Update starting_configuration_module.c

--- starting_configuration_module.c
+++ starting_configuration_module.c
@@ -1,34 +1,31 @@
-      /*
+/*
 **************************
 File Name:      starting_configuration_module.c
-Created By:
-Login:
+Created By:	Thomas Yang     
+Login:          thomasy4
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
 Last Modified:  26 November 2017
-Description:
+Description:	Creates and intializes the board given a selected difficulty.  Returns a board with mines laid and a preset safe zone.  
 **************************
 */
 
-/*      ***  Call Header Files  ***     */
+/*	***  Call Header Files  ***	*/
 #include <stdio.h>
 #include <stdlib.h>
-//#include "miensfeldutil.h"
-#include "display.h"
 #include <time.h>
+#include "display.h"
 #include "check_adj_module.h"
 #include "main_global.h"
 #include "print_shit.h"
-/*      ***  Define Compiler Directives  ***    */
-//#define DEBUG
-//#define DISPLAY
+#include "miensfeldutil.h"
 
-/*      ***  Functions Workspace  ***   */
+/*	***  Functions Workspace  ***	*/
 void starting_configuration_module(int level)
 {
-        #ifdef DEBUG
-        printf("DEBUG:\nI:\tlevel = %d", level);
-        #endif
+	#ifdef DEBUG
+	printf("DEBUG: ENTERING STARTING CONFIG\n\nlevel = %d\n\n", level);
+	#endif
 
    int mine, tempR, tempC, adjacent, i, j;
 
@@ -36,28 +33,32 @@ void starting_configuration_module(int level)
  draw_board();
 #endif
 
+
   if( level == 1 ) mine = 6;
   if( level == 2 ) mine = 11;
   if( level == 3 ) mine = 16;
   if( level == 4 ) mine = 20;
-
+ 
   //set score counter for mines and flags
    mines_count = mine;
    flags_count = mine;
 
-#ifdef DEBUG
-print_shit();
-#endif
-    
-      //Resets mine level
+variety = 0;
+
+
+  //Resets mine level
   for(i=0; i<8; i++)
   {
     for(j=0; j<10; j++)
     {
       mine_level[i][j] = EMPTY;
     }
-  }
+  } 
 
+#ifdef DEBUG
+printf("make mine level empty\n");
+print_shit();
+#endif
 
   //Resets timmy level
    for(i=0; i<8; i++)
@@ -66,13 +67,12 @@ print_shit();
     {
       timmy_level[i][j] = TIM_NOT_BEEN;
     }
-  }
+  } 
 
 #ifdef DEBUG
-printf("test1\n");
+printf("set timmy level to TIM_NOT_BEEN\n");
 print_shit();
 #endif
-
 
 
    //Resets timmy location
@@ -81,7 +81,7 @@ print_shit();
    timmy_level[0][0] = TIM_ON;
 
 #ifdef DEBUG
-printf("test2\n");
+printf("set timmy location\n");
 print_shit();
 #endif
 
@@ -90,17 +90,17 @@ print_shit();
   {
     for(j=0; j<10; j++)
     {
-      display_level[i][j] = 8;//EMPTY;
+      display_level[i][j] = EMPTY;
     }
   }
+
 #ifdef DEBUG
-printf("test3\n");
+printf("empty display level\n");
 print_shit();
 #endif
 
-
-  //Set safe for mine level
-  for(i=0; i<8; i++)
+    //Set safe for mine level
+  for(i=0; i<7; i++)
   {
     mine_level[i][0] = SAFE;
   }
@@ -113,7 +113,12 @@ print_shit();
     mine_level[3][2] = SAFE;
     mine_level[5][1] = SAFE;
 
-      srand(time(NULL));
+#ifdef DEBUG
+printf("set safe cells\n");
+print_shit();
+#endif
+
+srand(time(NULL));    
   //Sets mines randomly
   while(mine > 0)
   {
@@ -127,8 +132,13 @@ print_shit();
     }
   }
 
-  //Draws on display level for safe cells
+#ifdef DEBUG
+printf("distribute mines\n");
+print_shit();
+#endif
 
+  //Draws on display level for safe cells
+  
   for(i=0; i<8; i++)
   {
     for(j=0; j<10; j++)
@@ -136,21 +146,27 @@ print_shit();
       if(mine_level[i][j] == SAFE)
       {
         adjacent = adj(i,j);
-        #ifdef DISPLAY
+	#ifdef DISPLAY
         show_glif(SAFE,i,j,adjacent);
-        #endif
-        timmy_level[i][j] = TIM_BEEN;
+ 	#endif
+	timmy_level[i][j] = TIM_BEEN;
         display_level[i][j] = SAFE;
       }
       if(mine_level[i][j] == EMPTY || mine_level[i][j] == MINE)
-        #ifdef DISPLAY
+	{
+	#ifdef DISPLAY
         show_glif(EMPTY,i,j,0);
-        #endif
+	#endif
         display_level[i][j] = EMPTY;
+	}
     }
-  }
+  } 
 
-
+#ifdef DEBUG
+printf("re-configure display level\n");
+print_shit();
+#endif 
+  
   //Sets all other empty to safe
   for(i=0; i<8; i++)
   {
@@ -160,6 +176,12 @@ print_shit();
       mine_level[i][j] = SAFE;
     }
   }
+
+#ifdef DEBUG
+printf("make all unset cells safe\n");
+print_shit();
+#endif
+
   //Initializes scoreboard
   score_count = 0;
 #ifdef DISPLAY
@@ -174,5 +196,10 @@ adjacent = adj(0,0);
 show_glif(TIMMY,0,0,adjacent);
 #endif
 display_level[0][0] = TIMMY;
+
+#ifdef DEBUG
+printf("initialize score and draw timmy on board\n");
+print_shit();
+#endif
 
 }
