099865d6409da75117df2a336a41a35da0270dc0
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 00:31:17 -1000

    Update endgame_module.c

--- endgame_module.c
+++ endgame_module.c
@@ -1,12 +1,12 @@
 /*
 **************************
 File Name:      endgame_module.c
-Created By:     
-Login:          
+Created By:     Lian Huang
+Login:          lian7
 Team:           Undercooked Dinosaurs
-Date Created:   26 November 2017
-Last Modified:  26 November 2017
-Description:	  
+Date Created:   8 December 2017
+Last Modified:  8 December 2017
+Description:	this file contains the functions used if the play wins or looses, and then runs a sequence to quit.
 **************************
 */
 
@@ -22,84 +22,118 @@ Description:
 
 
 /*	***  Functions Workspace  ***	*/
+
+/* Function Name:  death
+ * I/O:  void / void
+ * Purpose/Notes:  This function runs the animation and message printing if Timmy dies*/
 void death(void)
 {
-#ifdef DISPLAY
-  int i, j;
-write_message(12, "");
-write_message(13, "");
-write_message(14, "");
-write_message(15, "");
-write_message(16, "");
+	#ifdef DISPLAY
+		//declare variables
+		int i, j;
+	
+		//clear write area
+		write_message(12, "");
+		write_message(13, "");
+		write_message(14, "");
+		write_message(15, "");
+		write_message(16, "");
 
-  for(i=0; i<8; i++)
-  {
-    for(j=0; j<10; j++)
-    {
-      if(mine_level[i][j] == MINE)
-      {
-        show_glif(MINE,i,j,0);
-      }
-    }
- }
-  show_glif(EXPLODE,timmys_location[0],timmys_location[1],0);
-  write_message(12, "We're gonna need");
-  write_message(13, "another Timmy!");
-#endif
-#ifdef DEBUG
-printf("\nHe died, meh life is pointless and meaningless\nand you have to do projects like this so Timmy\nprobably got the better end of the deal anyway\n");
-#endif
+		//show mines
+		for(i=0; i<8; i++)
+		{
+			for(j=0; j<10; j++)
+			{
+				if(mine_level[i][j] == MINE)
+				{
+					show_glif(MINE,i,j,0);
+				}
+			}
+		}
 
-  quit_sequence();
- }
+		//show Timmy's death
+		show_glif(EXPLODE,timmys_location[0],timmys_location[1],0);
+	
+		//print death message
+		write_message(12, "We're gonna need");
+		write_message(13, "another Timmy!");
+	#endif
+
+	#ifdef DEBUG
+	printf("\nHe died, meh life is pointless and meaningless\nand you have to do projects like this so Timmy\nprobably got the better end of the deal anyway\n");
+	#endif
+	
+	//run quit_sequence
+	quit_sequence();
+}
 
 
+/* Function Name:  win
+ * I/O:  void / void
+ * Purpose/Notes:  This function runs the animation and message printing if the player wins*/
 void win(void)
 {
-#ifdef DISPLAY
-  int i, j, adjacent;
+	#ifdef DISPLAY
+		//declare variables
+		int i, j, adjacent;
 
-write_message(12, "");
-write_message(13, "");
-write_message(14, "");
-write_message(15, "");
-write_message(16, "");
+		//clear write area
+		write_message(12, "");
+		write_message(13, "");
+		write_message(14, "");
+		write_message(15, "");
+		write_message(16, "");
 
-  for(i=0; i<8; i++)
-  {
-    for(j=0; j<10; j++)
-    {
-      if(mine_level[i][j] == MINE)
-      {
-        show_glif(MINE,i,j,0);
-      }
-    }
-  }
-  adjacent = adj(timmys_location[0],timmys_location[1]);
-  show_glif(SAFE,timmys_location[0],timmys_location[1],adjacent);
-  write_message(12, "Cognratuations!");
-  write_message(13, "You won!");
-#endif
-#ifdef DEBUG
-printf("\nYou won, yay we are so fucking pleased\n");
-#endif
-  quit_sequence();
+		//show mines
+		for(i=0; i<8; i++)
+		{
+			for(j=0; j<10; j++)
+			{
+				if(mine_level[i][j] == MINE)
+				{
+					show_glif(MINE,i,j,0);
+				}
+			}
+		}
 
+		//show Timmy is safe
+		adjacent = adj(timmys_location[0],timmys_location[1]);
+		show_glif(SAFE,timmys_location[0],timmys_location[1],adjacent);
+	
+		//write win message
+		write_message(12, "Cognratuations!");
+		write_message(13, "You won!");
+	#endif
+	
+	#ifdef DEBUG
+	printf("\nYou won, yay we are so fucking pleased\n");
+	#endif
+
+	//run quit_sequence
+	quit_sequence();
 }
-    
+
+
+/* Function Name:  quit_sequence
+ * I/O:  void / void
+ * Purpose/Notes:  This function asks the user if they want to play again and responds accordingly*/
 void quit_sequence(void)
 {
- 	char ch;
-write_message(12, "");
-write_message(13, "");
-write_message(14, "");
-write_message(15, "");
-write_message(16, "");
+	//declare variables
+	char ch;
 
-  
+	//clear write area
+	write_message(12, "");
+	write_message(13, "");
+	write_message(14, "");
+	write_message(15, "");
+	write_message(16, "");
+
+  	//ask if user wants to play again
 	#ifdef DEBUG
 	printf("\nPlay again bitch?\n");
 	#endif
+	
 	#ifdef DISPLAY
 	write_message(15, "Do you want to"); 
 	write_message(16, "play again?");
@@ -107,21 +141,25 @@ write_message(16, "");
 	write_message(18, " and N for no)\n");
 	#endif		
 
+	//while user has not yet entered a valid answer
 	while(TRUE)
 	{
+		//continuous run command line input
 		system("/bin/stty raw");
 		ch = getchar();
 		system("/bin/stty cooked");
 
+		//if yes restart game
 		if(ch == 'y' || ch == 'Y')
 		{
 			quit_flag = RESTART;
 			break;
 		}
+		
+		//if no the quit
 		if(ch == 'n' || ch == 'N')
 		{
 			quit_flag = TRUE;
-			system("clear");
 			break;
 		}
 	}
