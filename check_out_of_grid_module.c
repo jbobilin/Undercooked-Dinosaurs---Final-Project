274f6babc594e3eb1fc615b793c0792eabdd286c
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 00:07:10 -1000

    Update check_out_of_grid_module.c

--- check_out_of_grid_module.c
+++ check_out_of_grid_module.c
@@ -7,9 +7,9 @@ Team:		Undercooked Dinosaurs
 Date Created:	26 November 2017
 Last Modified:	1 December 2017
 Description:	This function is given a row and column and checks if the cell is within the playing area it returns
+		WIN_ZONE
 		OUT_OF_BOUNDS
 		IN_FIELD
-		WIN_ZONE
 **************************
 */
 
@@ -18,6 +18,10 @@ Description:	This function is given a row and column and checks if the cell is w
 #include "main_global.h"
 
 /*	***  Functions Workspace  ***	*/
+
+/* Function Name:  check_out_of_grid
+ * I/O:  int row, int column /  int
+ * Purpose/Notes:  This function is given a cell on the playing field and calculates if it is within the gameplay area*/
 int check_out_of_grid(int row, int column)
 {
 	// if column is within farest right column, return WIN_ZONE
