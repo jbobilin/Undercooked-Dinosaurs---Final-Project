28c36d3ebcdcb45a3b4501617a12afe9e5f6b705
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 00:32:49 -1000

    Update endgame_module.h

--- endgame_module.h
+++ endgame_module.h
@@ -1,11 +1,11 @@
 /*
 **************************
 File Name:      endgame_module.h
-Created By:     
-Login:          
+Created By:     Lian Huang
+Login:          lian7
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
-Last Modified:  1 December 2017
+Last Modified:  8 December 2017
 Description:	  
 **************************
 */
@@ -13,9 +13,19 @@ Description:
 
 /*	***  Declare Prototypes  ***	*/
 
+/* Function Name:  death
+ * I/O:  void / void
+ * Purpose/Notes:  This function runs the animation and message printing if Timmy dies*/
 void death(void);
+
+
+/* Function Name:  win
+ * I/O:  void / void
+ * Purpose/Notes:  This function runs the animation and message printing if the player wins*/
 void win(void);
+
+
+/* Function Name:  quit_sequence
+ * I/O:  void / void
+ * Purpose/Notes:  This function asks the user if they want to play again and responds accordingly*/
 void quit_sequence(void);
-/* Function Name:  
- * I/O:  /  
- * Purpose/Notes:  */
