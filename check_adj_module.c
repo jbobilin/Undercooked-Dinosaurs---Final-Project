e6cfb43e04043de650341b8606f5815fffb8d3b4
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-08 22:46:33 -1000

    Update check_adj_module.c

--- check_adj_module.c
+++ check_adj_module.c
@@ -5,16 +5,17 @@ Created By:     Jonah Bobilin
 Login:          jbobilin
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
-Last Modified:	30 November 2017
-Description:	This file contains the function adj.
+Last Modified:  30 November 2017
+Description:	  This file contains the function adj.
 **************************
 */
 
 /*	***  Call Header Files  ***	*/
-//#include "miensfeldutil.h"
+
 #include "display.h"
 #include "main_global.h"
-
+#include "miensfeldutil_global.h"
+#include "check_out_of_grid_module.h"
 /*	***  Functions Workspace  ***	*/
 
 /* Function Name:  adj
@@ -24,23 +25,52 @@ int adj(int row, int column)
 {
 	//declare and initialize count variable
 	int count = 0;    /* variable to count mines in a adjacent tiles */
-    
-	if (MINE == mine_level[row - 1][column - 1])	//check up left cell for mine
-		count++;
-	if MINE == mine_level[row - 1]][column])		//check up cell for mine
-		count++;
-	if (MINE == mine_level[row - 1][column + 1])	//check up right cell for mine
-		count++;
-	if (MINE == mine_level[row][column - 1])		//check left cell for mine
-		count++;
-	if (MINE == mine_level[row][column + 1])		//check right cell for mine
-		count++;
-	if (MINE == mine_level[row + 1][column - 1])	//check down left cell for mine
-		count++;
-	if (MINE == mine_level[row + 1][column])		//check down cell for mine
-		count++;
-	if (MINE == mine_level[row + 1][column + 1])	//check down right cell for mine
-		count++;
+    	if (check_out_of_grid(row - 1, column - 1) != OUT_OF_BOUNDS && row > 1 && column > 0)
+	{
+		if (MINE == mine_level[row - 1][column - 1] || FL_MINE == mine_level[row - 1][column - 1])	//check up left cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row - 1, column) != OUT_OF_BOUNDS && row > 0)
+	{
+		if (MINE == mine_level[row - 1][column] || FL_MINE == mine_level[row - 1][column])		//check up cell for mine
+			count++;
+	}
+	if (check_out_of_grid(row - 1, column + 1) != OUT_OF_BOUNDS && row > 0 && column < 9)
+	{
+		if (MINE == mine_level[row - 1][column + 1] || FL_MINE == mine_level[row - 1][column + 1])	//check up right cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row, column - 1) != OUT_OF_BOUNDS && column > 0)
+	{
+		if (MINE == mine_level[row][column - 1] || FL_MINE == mine_level[row][column - 1])		//check left cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row, column + 1) != OUT_OF_BOUNDS && column < 9)
+	{
+		if (MINE == mine_level[row][column + 1] || FL_MINE == mine_level[row][column + 1])		//check right cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row + 1, column - 1) != OUT_OF_BOUNDS && row < 7 && column > 0)
+	{
+		if (MINE == mine_level[row + 1][column - 1] || FL_MINE == mine_level[row + 1][column - 1])	//check down left cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row + 1, column) != OUT_OF_BOUNDS && row < 7)
+	{
+		if (MINE == mine_level[row + 1][column] || FL_MINE == mine_level[row + 1][column])		//check down cell for mine
+			count++;
+	}
+
+	if (check_out_of_grid(row + 1, column + 1) != OUT_OF_BOUNDS && row < 7 && column < 9)
+	{
+		if (MINE == mine_level[row + 1][column + 1] || FL_MINE == mine_level[row + 1][column + 1])	//check down right cell for mine
+			count++;
+	}
  
 	//return total mines counted in adjacent cells
 	return count;
