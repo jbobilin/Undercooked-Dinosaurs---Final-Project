36655086cfa4510cbe8b4bc75d1aa701229f696e
Asher Flagon <33672661+jbobilin@users.noreply.github.com>
2017-12-09 01:12:32 -1000

    Update move_module.h

--- move_module.h
+++ move_module.h
@@ -1,12 +1,12 @@
 /*
 **************************
 File Name:      move_module.h
-Created By:     
-Login:          
+Created By:     Thomas Yang
+Login:          thomasy4
 Team:           Undercooked Dinosaurs
 Date Created:   26 November 2017
 Last Modified:  26 November 2017
-Description:	  
+Description:	this file contains the prototype for the move_tim function
 **************************
 */
 
